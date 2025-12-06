d7a80fbb06db4a97605e74b0c214dd09050ab75b
SachiniUresha <dsuweerakkody19@gmail.com>
2025-12-06 23:47:02 +0530

    Testing - Log out

--- src/main/java/Lauchguru.java
+++ src/main/java/Lauchguru.java
@@ -2,9 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 
 public class Lauchguru {
@@ -99,7 +97,9 @@ public class Lauchguru {
         driver.findElement(By.name("password")).sendKeys("123456789");
         driver.findElement(By.name("submit")).click();
 
-        //driver.findElement(By.linkText("Manager")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/form/input")).click();
+
+
 
 
 
